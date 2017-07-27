380069f80fafaf3f86a4b171f5e0a3c5d86c3850
kareem atef <kareem.atef@yahoo.com>
2017-07-27 16:09:01 +0200

    Update 10 HTTP File Server.js

--- 10 HTTP File Server.js
+++ 10 HTTP File Server.js
@@ -4,7 +4,7 @@ var port = process.argv[2];
 var dirPath = process.argv[3];
 
 var server = http.createServer(function(request,response){
-    response.writeHead(200,{'content-type':'text/plain'});
+    //response.writeHead(200,{'content-type':'text/plain'});
     var src = fs.createReadStream(dirPath);
     src.pipe(response);
 });
